3a275d1e222d774fe3aca33af93141066c79dbd0
wuziyong <wuzy55@mail2.sysu.edu.cn>
2021-06-17 23:13:48 +0800

    添加登录系统

--- SignIn/SignIn/urls.py
+++ SignIn/SignIn/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': settings.MEDIA_ROOT}, name = 'static'),
     path('', include('apps.sign.urls')),
     path('', include('apps.file.urls')),
+    path('', include('apps.login.urls')),
 ]
